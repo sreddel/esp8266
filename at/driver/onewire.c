dcf944de57d161c5998cf81a2a1bfdae8b720dd7
Your Name <you@example.com>
2014-10-15 13:37:28 +0100

    fleshing out onewire driver functions

--- at/driver/onewire.c
+++ at/driver/onewire.c
@@ -3,10 +3,12 @@
 #include "driver/onewire.h"
 
 #include "gpio.h"
+#include "ets_sys.h"
 
 LOCAL uint8_t onewire_pin;
 
-void onewire_init (uint8_t pin) { 
+void onewire_init (uint8_t pin)
+	{
 
 
 	onewire_pin = pin;
@@ -25,3 +27,97 @@ void onewire_init (uint8_t pin) {
 
 
 	}
+
+uint8 onewire_reset(void) 
+	{
+
+		uint8 r;
+		uint8 retries = 125;
+
+		GPIO_DIS_OUTPUT(onewire_pin);
+
+		while (!GPIO_INPUT_GET(onewire_pin))
+		{
+			if (--retries==0)
+			{
+				return 0;
+			}
+			esp8266_sleep(2);
+		}
+
+		ETS_UART_INTR_DISABLE();
+		ETS_FRC1_INTR_DISABLE();
+		ETS_GPIO_INTR_DISABLE();
+
+		GPIO_OUTPUT_SET(GPIO_PIN_ADDR(onewire_pin),0);
+		esp8266_sleep(480);
+
+		GPIO_DIS_OUTPUT(onewire_pin);
+		esp8266_sleep(70);
+		r = !GPIO_INPUT_GET(onewire_pin);
+
+		ETS_UART_INTR_ENABLE();
+		ETS_FRC1_INTR_ENABLE();
+		ETS_GPIO_INTR_ENABLE();
+
+		esp8266_sleep(410);
+
+		GPIO_OUTPUT_SET(GPIO_PIN_ADDR(onewire_pin),1);
+
+		return r;
+
+	}
+
+void onewire_writebit(uint8 v)
+{
+	if (v & 1)
+	{
+		ETS_UART_INTR_DISABLE();
+		ETS_FRC1_INTR_DISABLE();
+		ETS_GPIO_INTR_DISABLE();
+		GPIO_OUTPUT_SET(GPIO_PIN_ADDR(onewire_pin),0);
+		esp8266_sleep(10);
+		GPIO_OUTPUT_SET(GPIO_PIN_ADDR(onewire_pin),1);
+		ETS_UART_INTR_ENABLE();
+		ETS_FRC1_INTR_ENABLE();
+		ETS_GPIO_INTR_ENABLE();
+		esp8266_sleep(55);
+	}
+	else
+	{
+		ETS_UART_INTR_DISABLE();
+		ETS_FRC1_INTR_DISABLE();
+		ETS_GPIO_INTR_DISABLE();
+		GPIO_OUTPUT_SET(GPIO_PIN_ADDR(onewire_pin),0);
+		esp8266_sleep(65);
+		GPIO_OUTPUT_SET(GPIO_PIN_ADDR(onewire_pin),1);
+		ETS_UART_INTR_ENABLE();
+		ETS_FRC1_INTR_ENABLE();
+		ETS_GPIO_INTR_ENABLE();
+		esp8266_sleep(5);
+	}
+}
+
+uint8 onewire_readbit(void)
+{
+	uint8 r;
+
+	ETS_UART_INTR_DISABLE();
+	ETS_FRC1_INTR_DISABLE();
+	ETS_GPIO_INTR_DISABLE();
+	
+	GPIO_OUTPUT_SET(GPIO_PIN_ADDR(onewire_pin),0);
+	esp8266_sleep(3);
+	GPIO_DIS_OUTPUT(onewire_pin);
+	esp8266_sleep(10);
+	r = GPIO_INPUT_GET(onewire_pin);
+
+	ETS_UART_INTR_ENABLE();
+	ETS_FRC1_INTR_ENABLE();
+	ETS_GPIO_INTR_ENABLE();
+
+	esp8266_sleep(53);
+
+	return r;
+
+}
--- at/include/driver/onewire.h
+++ at/include/driver/onewire.h
@@ -6,4 +6,33 @@
 #define ONEWIRE_SEARCH 1
 #endif
 
-void onewire_init (uint8_t pin);
+void onewire_init(uint8 pin);
+
+uint8 onewire_reset(void);
+
+void onewire_writebit(uint8 v);
+
+uint8 onewire_readbit(void);
+
+void onewire_write(uint8 v, bool power);
+
+void onewire_writebytes(uint16 *str, uint8 length, bool power);
+
+uint8 onewire_read(void);
+
+void onewire_readbytes(uint16 *str, uint8 length);
+
+void onewire_select(uint16 rom[8]);
+
+void onewire_skip(void);
+
+void onewire_depower(void);
+
+void onewire_resetsearch(void);
+
+void onewire_targetsearch(uint8 family_code);
+
+uint8 onewire_search(uint16 *newAddr);
+
+uint8 onewire_crc8(uint8 *str, uint8 length);
+
